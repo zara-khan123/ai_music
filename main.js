983f0f77c2429cf6e4f4ce62158cbe54aee1ba7a
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-04-01 18:42:16 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = "";
 song2 = ""
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -14,9 +18,44 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+
+
+function modelLoaded()
+{
+    console.log('posenet is intialized');
 }
 
 function draw()
 {
     image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    console.log(results);
+    
+    if(results.length > 0)
+    {
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " + leftWristX +"leftWristY = " + leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " + rightWristX +"rightWristY = " + rightWristY);
+    }
+
+    
 }
