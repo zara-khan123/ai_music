b2663540ffd21a605b7c329db45c0048772cd28d
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2023-04-04 12:20:31 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 scoreleftWrist = 0;
+scorerightWrist = 0;
 updatesong1 = "";
 updatesong2 = "";
 
@@ -51,6 +52,18 @@ function draw()
      document.getElementById("song").innerHTML = "playing peter pan";
     }
     }
+    
+    if(scorerightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+    if( updatesong2 == false)
+    {
+     song2.play();
+     document.getElementById("song").innerHTML = "playing harry potter";
+    }
+    }
+    
 }
 
 function play()
@@ -67,9 +80,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-
+        scorerightWrist = results[0].pose.keypoints[10].score;
         scoreleftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreleftWrist = "+ scoreleftWrist); 
+        console.log("scoreleftWrist = "+ scoreleftWrist+ "scorerightWrist = "+ scorerightWrist); 
 
     leftWristX = results[0].pose.leftWrist.x;
     leftWristY = results[0].pose.leftWrist.y;
